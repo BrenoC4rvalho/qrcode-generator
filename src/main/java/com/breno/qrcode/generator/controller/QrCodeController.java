26ad71a7e97d8f0a6bad54e1244006d87d5407c3
BrenoC4rvalho <bc101004@gmail.com>
2025-05-04 23:04:46 -0300

    feat: implement method generate.

--- src/main/java/com/breno/qrcode/generator/controller/QrCodeController.java
+++ src/main/java/com/breno/qrcode/generator/controller/QrCodeController.java
@@ -2,6 +2,7 @@ package com.breno.qrcode.generator.controller;
 
 import com.breno.qrcode.generator.dto.QrCodeGeneratorRequest;
 import com.breno.qrcode.generator.dto.QrCodeGeneratorResponse;
+import com.breno.qrcode.generator.service.QrCodeGeneratorService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,8 +13,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/qrcode")
 public class QrCodeController {
 
+    private final QrCodeGeneratorService qrCodeGeneratorService;
+
+    public QrCodeController(QrCodeGeneratorService qrCodeGeneratorService) {
+        this.qrCodeGeneratorService = qrCodeGeneratorService;
+    }
+
     @PostMapping
     public ResponseEntity<QrCodeGeneratorResponse> generate(@RequestBody QrCodeGeneratorRequest request) {
-        return null;
+        try {
+            QrCodeGeneratorResponse response = this.qrCodeGeneratorService.generateAndUploadQrCode(request.text());
+            return ResponseEntity.ok(response);
+        } catch (Exception e) {
+            return ResponseEntity.internalServerError().build();
+        }
+
+
     }
 }
